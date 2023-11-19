0d6dc1959f1a2f3358ce892e7080e72990b69537
Johnathan Kinslow <superxtenorxguy117@gmail.com>
2023-11-19 12:45:43 -0500

    initial

---
+++ WarhammerGameManager.Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WarhammerGameManager.Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
