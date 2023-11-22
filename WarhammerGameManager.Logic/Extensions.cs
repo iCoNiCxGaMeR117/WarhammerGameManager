7921b0d25ac8a2916b5827be565c738c82c2e6d9
Johnny Kinslow <Johnny.Kinslow@WesternSouthernLife.com>
2023-11-22 08:22:33 -0500

    adding in some charting logic and mapping

---
+++ WarhammerGameManager.Logic/Extensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WarhammerGameManager.Entities.ViewModels;
+
+namespace WarhammerGameManager.Logic
+{
+    public static class Extensions
+    {
+        public static string GenerateBasicChartData(this Dictionary<string, decimal> data)
+        {
+                return JsonSerializer.Serialize(data
+                    .Select(x =>
+                        new ChartingModel
+                        {
+                            Key = x.Key,
+                            Value = x.Value
+                        })
+                    .Where(x => x.Value > 0)
+                    .ToArray());
+        }
+
+        public static string GenerateListChartData<T>(this Dictionary<string, IList<T>> data)
+        {
+            return JsonSerializer.Serialize(data
+                .Select(x =>
+                    new ChartingModel
+                    {
+                        Key = x.Key,
+                        Value = x.Value.Count
+                    })
+                .Where(x => x.Value > 0)
+                .ToArray());
+        }
+    }
+}
